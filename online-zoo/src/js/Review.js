d995269be090d3e9c9809e5b79b1a959ef1ad865
Anastasia Sytsevich <drozdushka@gmail.com>
2022-10-09 01:52:58 +0300

    feat: generate reviews

--- online-zoo/src/js/Review.js
+++ online-zoo/src/js/Review.js
@@ -1,3 +1,86 @@
+ import dataReview from "./dataReview";
+ 
+
+ //-----------------Alexey
+
+//  export default class Review {
+
+//     /**
+//      * 
+//      * @param {object[]} reviews
+//      * @param {HTMLElement} parent
+//      * 
+//      */
+//     constructor(reviews, parent) { 
+//         reviews.forEach( (review) => {
+//             const reviewHTML =  this.generateReview(review);
+//             const element = document.createElement('div');
+//             element.innerHTML = reviewHTML;
+//             parent.append(element);
+//         });
+//     }
+//     generateReview(reviewItem) {
+//         return `
+//             <div class="gradient_border" data-id="${reviewItem.id}">
+//                 <div class="testimonials_card">
+//                     <div class="testimonials_card_head">
+//                         <img src="${reviewItem.avatar}" alt="user" class="avatar">
+//                         <div class="title_content">
+//                             <p class="testimonials_title_card">${reviewItem.nameUser}</p>
+//                             <p class="testimonials_subtitle">Local ${reviewItem.local} &bull; Today</p>
+//                         </div>
+//                     </div>
+//                         <p class="testimonials_text">${reviewItem.content}<</p>
+//                 </div>
+//             </div>
+//         `;
+//     }
+//  }
+
+
+
+//-----------------------Vika
+
 export class Review {
-    
+    constructor({ id, nameUser, local, content, avatar, ...rest }) {
+        this.id = id;
+        this.nameUser = nameUser;
+        this.local = local;
+        this.content = content;
+        this.avatar = avatar;
+    }
+
+    //reviews generator
+    generateReview() {
+        let template = '';
+        let review = document.createElement('review');
+        review.className = 'gradient_border';
+        review.setAttribute('data-id', this.id);
+
+        template += `<div class="testimonials_card">`
+            template += `<div class="testimonials_card_head">`
+
+                this.avatar &&
+                (template += `<img src=${this.avatar} alt="user" class="avatar">`)
+
+                template += `<div class="title_content">`
+
+                    this.nameUser &&
+                    (template += `<p class="testimonials_title_card">${this.nameUser}</p>`)
+
+                    this.local &&
+                    (template += `<p class="testimonials_subtitle">Local ${this.local} &bull; Today</p>`)
+
+                template += `</div>`
+
+            template += `</div>`
+
+            this.content &&
+            (template += `<p class="testimonials_text"> ${this.content} </p>`)
+
+        template += `</div>`
+
+        review.innerHTML = template;
+        return review;
+    }
 }
---
+++ online-zoo/src/js/dataReview.js
@@ -0,0 +1,83 @@
+const dataReview = [
+    {
+        id: 1,
+        nameUser: 'Michael John',
+        local: 'Austria',
+        content: 'We decided to visit this famous Zoo (one of the oldest in the world) over the Easter weekend on Good Friday. We decided to purchase the combo tickets which allows access both to the Zoo itself an stage Tropical Aquarium. Tickets were not cheap €40 each for one adult. We began our visit by going into the Tropical Aquarium first and I have to say this was the best part of the overall experience. I\'ve never seen seen such hugh aquariums before it was amazing. The Zoo or tierpark itself was OK but I found that some of the enclosures were rather small for some of the bigger animals ,e.g. Elephants. So a bit disappointed with that in have to say. I guess you have to remember that the City of Hamburg has encroached upon the park over many years which limits development, hence a 4 and not a 5. If I were to visit again I would certainty go to the Tropical Aquarium that was impressive, but the Zoo no.',
+        avatar: '../../assets/images/avatars/Ellipse 1.jpg',
+    },
+    {
+        id: 2,
+        nameUser: 'Irene Rubenyan',
+        local: 'Greece',
+        content: 'It was a great experience going to the zoo park Hagenbeck!! We had a little time when we went so we chose to get in the aquarium!! It was so clean, interesting and it had a big sort of animals!! It was like you were in a discovering were its every animal and it was more exciting like that!! You were close to some of them!! ',
+        avatar: '../../assets/images/avatars/Ellipse 2.png',
+    },
+    {
+        id: 3,
+        nameUser: 'Phillip D',
+        local: 'Belarus',
+        content: 'Came to the zoo as we alway try and visit one when on holiday. Lots of animals to see, particularly like the penguins, seals and walrus\’ It was a cold day so lots of the animals where trying to keep warm, but still plenty to see.Also visited the aquarium,it was nice but we have been to better, but plenty to look at.',
+        avatar: '../../assets/images/avatars/Ellipse 3.png',
+    },
+    {
+        id: 4,
+        nameUser: 'Adrian L',
+        local: 'China',
+        content: 'My godson and his little brother really love zoos and their parents and I brought them to this famous zoo in Germany. The parents told me that they had the best array of animals, and they were allowed to feed certain animals such as monkeys, elephants and lamas. They have stalls providing food after passing the entrance and the min-car/trolley area. There are no fees but you are encouraged to make a donations, which most people do. Even one or two euros are appreciated by the staff. The kids have a fun times feeding the elephants and other animals.',
+        avatar: '../../assets/images/avatars/Ellipse 4.png',
+    },
+
+    {
+        id: 5,
+        nameUser: 'Michael John',
+        local: 'Austria',
+        content: 'We decided to visit this famous Zoo (one of the oldest in the world) over the Easter weekend on Good Friday. We decided to purchase the combo tickets which allows access both to the Zoo itself an stage Tropical Aquarium. Tickets were not cheap €40 each for one adult. We began our visit by going into the Tropical Aquarium first and I have to say this was the best part of the overall experience. I\'ve never seen seen such hugh aquariums before it was amazing. The Zoo or tierpark itself was OK but I found that some of the enclosures were rather small for some of the bigger animals ,e.g. Elephants. So a bit disappointed with that in have to say. I guess you have to remember that the City of Hamburg has encroached upon the park over many years which limits development, hence a 4 and not a 5. If I were to visit again I would certainty go to the Tropical Aquarium that was impressive, but the Zoo no.',
+        avatar: '../../assets/images/avatars/Ellipse 5.jpg',
+    },
+    {
+        id: 6,
+        nameUser: 'Irene Rubenyan',
+        local: 'Greece',
+        content: 'It was a great experience going to the zoo park Hagenbeck!! We had a little time when we went so we chose to get in the aquarium!! It was so clean, interesting and it had a big sort of animals!! It was like you were in a discovering were its every animal and it was more exciting like that!! You were close to some of them!! ',
+        avatar: '../../assets/images/avatars/Ellipse 6.jpg',
+    },
+    {
+        id: 7,
+        nameUser: 'Phillip D',
+        local: 'Belarus',
+        content: 'Came to the zoo as we alway try and visit one when on holiday. Lots of animals to see, particularly like the penguins, seals and walrus\’ It was a cold day so lots of the animals where trying to keep warm, but still plenty to see.Also visited the aquarium,it was nice but we have been to better, but plenty to look at.',
+        avatar: '../../assets/images/avatars/Ellipse 7.jpg',
+    },
+    {
+        id: 8,
+        nameUser: 'Alexey',
+        local: 'China',
+        content: 'My godson and his little brother really love zoos and their parents and I brought them to this famous zoo in Germany. The parents told me that they had the best array of animals, and they were allowed to feed certain animals such as monkeys, elephants and lamas. They have stalls providing food after passing the entrance and the min-car/trolley area. There are no fees but you are encouraged to make a donations, which most people do. Even one or two euros are appreciated by the staff. The kids have a fun times feeding the elephants and other animals.',
+        avatar: '../../assets/images/avatars/Ellipse 8.jpg',
+    },
+    {
+        id: 9,
+        nameUser: 'Irene Rubenyan',
+        local: 'Greece',
+        content: 'It was a great experience going to the zoo park Hagenbeck!! We had a little time when we went so we chose to get in the aquarium!! It was so clean, interesting and it had a big sort of animals!! It was like you were in a discovering were its every animal and it was more exciting like that!! You were close to some of them!! ',
+        avatar: '../../assets/images/avatars/Ellipse 9.jpg',
+    },
+    {
+        id: 10,
+        nameUser: 'Timosha',
+        local: 'Sweden',
+        content: 'Came to the zoo as we alway try and visit one when on holiday. Lots of animals to see, particularly like the penguins, seals and walrus\’ It was a cold day so lots of the animals where trying to keep warm, but still plenty to see.Also visited the aquarium,it was nice but we have been to better, but plenty to look at.',
+        avatar: '../../assets/images/avatars/Ellipse 10.jpg',
+    },
+    {
+        id: 11,
+        nameUser: 'Alexey',
+        local: 'China',
+        content: 'My godson and his little brother really love zoos and their parents and I brought them to this famous zoo in Germany. The parents told me that they had the best array of animals, and they were allowed to feed certain animals such as monkeys, elephants and lamas. They have stalls providing food after passing the entrance and the min-car/trolley area. There are no fees but you are encouraged to make a donations, which most people do. Even one or two euros are appreciated by the staff. The kids have a fun times feeding the elephants and other animals.',
+        avatar: '../../assets/images/avatars/Ellipse 11.jpg',
+    },
+    
+]
+
+export default dataReview
