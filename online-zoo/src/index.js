f1c0b5c638a516bbf18bfc33630c1064065df551
Anastasia Sytsevich <drozdushka@gmail.com>
2022-10-09 20:41:38 +0300

    feat: add amount slider and range slider

--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -1,11 +1,10 @@
 import {Review} from './js/Review';
 import dataReview from './js/dataReview';
-
-
-
+import dataPhothAnimals from './js/dataPhotoAnimal';
+import {pages} from './js/Pages';
 
 window.onload = function()  {
-  console.log("Hello!");
+    console.log("Hello!");
 }
 
 //--------------- Burger menu ---------------
@@ -23,59 +22,85 @@ window.onload = function()  {
 
 
     function openMenu() {
-    menuBg.classList.add('active');
-    menu.classList.add('active');
-     }
+        menuBg.classList.add('active');
+        menu.classList.add('active');
+    }
 
         
     function closeMenu() {
-    menuBg.classList.remove('active');
-    menu.classList.remove('active');
-     }
+        menuBg.classList.remove('active');
+        menu.classList.remove('active');
+    }
 
 }());
 
-//----------Писали с Лешей------------
 
-// const reviewParent = document.getElementById('test_one');
-
-// if (reviewParent) {
-//   new Review(dataReview, reviewParent);
-// }
-
-//----------Виктория Ворожун------------
+//----------Range slider------------
 
 //Render reviews
 
-if(dataReview) {
-  renderReviewsToDom();
+const getWindowType = () => {
+    if (window.innerWidth <= 1000 && window.innerWidth > 640) {
+        return 'small';
+    }
+    return 'big';
 }
 
+const reviewParent = document.getElementById('test_one');
 
-function renderReviewsToDom() {
-  const reviewsWrapper = getReviewsWrapper();
-  generateReviews(dataReview).forEach( review => {
-    reviewsWrapper.append(review.generateReview())
-  });
+if (reviewParent) {
+    console.log(window.innerWidth);
+    const review = new Review(dataReview, reviewParent, getWindowType());
+    const reviewSlider = document.getElementById('reviewSlider');
+    reviewSlider.setAttribute('max', review.getReviewsCount());
+    reviewSlider.addEventListener('change', (event) => {
+        const currentValue = event.target.value;
+        review.setCurrentActive(currentValue);
+    });
+    window.addEventListener('resize', () => {
+        const type = getWindowType();
+        review.changeStepsCount(type);
+    })
 }
 
-function getReviewsWrapper() {
-  const reviewsContainer = document.querySelector('.testimonials_wrapper');
-  reviewsContainer.innerHTML = '';
-  return reviewsContainer;
-}
+//---------------Amount sider ---------
+
+const progressBar = document.getElementById('progress');
+const inputs = progressBar.getElementsByTagName('input');
+
+const amounts = Array.prototype.reduce.call(inputs, (acc, input) => {
+    const value = input.getAttribute('value');
+    acc[value] = input;
+    return acc;
+}, {});
+
+const amountInput = document.getElementById('amount');
+progressBar.addEventListener('change', (event) => {
+    const currentRadio = event.target.value;
+    amountInput.setAttribute('value', currentRadio);
+});
+
+amountInput.addEventListener('change', (event) => {
+    const value = event.target.value;
+    const radioInput = amounts[value];
+    if (radioInput) {
+        radioInput.setAttribute('checked', true);
+    }
+});
+
+amountInput.addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[^\d]/gm, '');
+});
 
 
-/**
-//      * 
-//      * @param {object[]} dataReview
-//      * 
-//      */
-function generateReviews(dataReview) {
-  let reviews = [];
-  dataReview.forEach(review => {
-    reviews.push(new Review(review))
-  });
-  return reviews;
-}
+amountInput.setAttribute('value', 100);
+amounts['100'].setAttribute('checked', true);
+
+
+
+//-----------------Slider pets-----------------
+
+
+const sliderPet = document.getElementById('pet_slider');
+
 
--- online-zoo/src/js/Review.js
+++ online-zoo/src/js/Review.js
@@ -1,86 +1,68 @@
- import dataReview from "./dataReview";
- 
-
- //-----------------Alexey
-
-//  export default class Review {
-
-//     /**
-//      * 
-//      * @param {object[]} reviews
-//      * @param {HTMLElement} parent
-//      * 
-//      */
-//     constructor(reviews, parent) { 
-//         reviews.forEach( (review) => {
-//             const reviewHTML =  this.generateReview(review);
-//             const element = document.createElement('div');
-//             element.innerHTML = reviewHTML;
-//             parent.append(element);
-//         });
-//     }
-//     generateReview(reviewItem) {
-//         return `
-//             <div class="gradient_border" data-id="${reviewItem.id}">
-//                 <div class="testimonials_card">
-//                     <div class="testimonials_card_head">
-//                         <img src="${reviewItem.avatar}" alt="user" class="avatar">
-//                         <div class="title_content">
-//                             <p class="testimonials_title_card">${reviewItem.nameUser}</p>
-//                             <p class="testimonials_subtitle">Local ${reviewItem.local} &bull; Today</p>
-//                         </div>
-//                     </div>
-//                         <p class="testimonials_text">${reviewItem.content}<</p>
-//                 </div>
-//             </div>
-//         `;
-//     }
-//  }
-
-
-
-//-----------------------Vika
-
 export class Review {
-    constructor({ id, nameUser, local, content, avatar, ...rest }) {
-        this.id = id;
-        this.nameUser = nameUser;
-        this.local = local;
-        this.content = content;
-        this.avatar = avatar;
+
+    /**
+     * 
+     * @param {object[]} reviews
+     * @param {HTMLElement} parent
+     * 
+     */
+    constructor(reviews, parent, currentSize) {
+        this.reviews = reviews.map((review, index) => {
+            const reviewHTML =  this.generateReview(review);
+            const element = document.createElement('div');
+            element.classList.add('gradient_border');
+            element.setAttribute('data-id', review.id);
+            element.innerHTML = reviewHTML;
+            parent.append(element);
+
+            return element;
+        });
+
+        this.changeStepsCount(currentSize);
+        this.setCurrentActive(1)
     }
 
-    //reviews generator
-    generateReview() {
-        let template = '';
-        let review = document.createElement('review');
-        review.className = 'gradient_border';
-        review.setAttribute('data-id', this.id);
+    getReviewsCount() {
+        return this.stepsCount;
+    }
 
-        template += `<div class="testimonials_card">`
-            template += `<div class="testimonials_card_head">`
+    setCurrentActive(value) {
+        this.currentActive = Number.parseInt(value);
+        const highLimit = this.currentActive + this.screenLimit;
+        this.reviews.forEach((review, index) => {
+            review.classList.remove('active');
+            if (this.currentActive <= index && index < highLimit) {
+                review.classList.add('active');
+            }
+        });
+    }
 
-                this.avatar &&
-                (template += `<img src=${this.avatar} alt="user" class="avatar">`)
+    /**
+     * 
+     * @param {string} screenType 
+     */
+    changeStepsCount(screenType) {
+        this.screenLimit = screenType === 'small' ? 3 : 4;
+        this.stepsCount = this.reviews.length - 1 - this.screenLimit;
+        if (this.currentActive > this.stepsCount) {
+           this.setCurrentActive(this.stepsCount);
+        } else {
+            this.setCurrentActive(this.currentActive);
+        }
+    }
 
-                template += `<div class="title_content">`
-
-                    this.nameUser &&
-                    (template += `<p class="testimonials_title_card">${this.nameUser}</p>`)
-
-                    this.local &&
-                    (template += `<p class="testimonials_subtitle">Local ${this.local} &bull; Today</p>`)
-
-                template += `</div>`
-
-            template += `</div>`
-
-            this.content &&
-            (template += `<p class="testimonials_text"> ${this.content} </p>`)
-
-        template += `</div>`
-
-        review.innerHTML = template;
-        return review;
+    generateReview(reviewItem) {
+        return `
+            <div class="testimonials_card">
+                <div class="testimonials_card_head">
+                    <img src="${reviewItem.avatar}" alt="user" class="avatar">
+                    <div class="title_content">
+                        <p class="testimonials_title_card">${reviewItem.nameUser}</p>
+                        <p class="testimonials_subtitle">Local ${reviewItem.local} &bull; Today</p>
+                    </div>
+                </div>
+                    <p class="testimonials_text">${reviewItem.content}<</p>
+            </div>
+        `;
     }
 }
