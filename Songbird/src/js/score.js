ca898258c5b71f98dea0131426bb64447736e72c
Anastasia Sytsevich <drozdushka@gmail.com>
2022-11-21 00:46:33 +0300

    feat: add function show result

--- Songbird/src/js/score.js
+++ Songbird/src/js/score.js
@@ -3,9 +3,17 @@ const repeatButton = document.querySelector(".result_button");
 function getScore() {
   const data = sessionStorage.getItem('score');
   const result = document.querySelector('.result');
-
-  result.textContent = '';
-  result.textContent = `Ваш результат ${data} очков`
+  const message = document.querySelector('.message');
+console.log(data);
+  if (+data === 30) {
+    message.textContent = '';
+    message.textContent = `Игра окончена! Поздравляем!
+                           Вы набрали максимальное количество очков`;
+    result.textContent ='';
+  } else {
+    result.textContent = '';
+    result.textContent = `Ваш результат ${data} очков`
+  }
 }
 
 getScore()
