9285d480446a983fa3d7f265298d2cbcdf21e6a0
Anastasia Sytsevich <drozdushka@gmail.com>
2022-12-08 21:47:40 +0300

    refactor: change key in appLoader.ts

--- Migration to TypeScript/src/components/controller/appLoader.ts
+++ Migration to TypeScript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '8959bf591fa74fa9a5b19147ebdb584f', // получите свой ключ https://newsapi.org/
+            apiKey: '837676c4ddd4443d8926ea0713b77103', // получите свой ключ https://newsapi.org/
         });
     }
 }
